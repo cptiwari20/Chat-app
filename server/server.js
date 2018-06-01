634d3fb6310c2e7e292c3642a2b9bca63f1724e4
cptiwari20 <cptiwari20@gmail.com>
2018-06-01 20:10:39 +0530

    inital commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path    = require('path');
+var publicPath = path.join(__dirname + "../public");
+var Port       = process.env.PORT || 3000;
+var app        = express();
+app.use(express.static(publicPath));
+
+app.listen(Port, (req, res)=> {
+    console.log('Server has been started; i.e.', Port);
+});
+
