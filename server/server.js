0acaec6986ef87056c27bcc3044e626d3ec10f4e
cptiwari20 <cptiwari20@gmail.com>
2018-06-05 18:39:48 +0530

    adding brodcast emit and event listeners

--- server/server.js
+++ server/server.js
@@ -13,13 +13,28 @@ io.on('connection', function(socket){
     console.log('New User Connected');
 
     socket.emit('newMessage', {
-        from: "vikas@example.com",
-        text: "Hey! whats up",
-        createdAt: new Date()
+        from: "Admin",
+        text: "Welcome to the Chat room!",
+        createdAt: new Date().getTime()
     });
+    socket.broadcast.emit("newMessage", {
+        from: "Admin",
+        text: "A new user connected!!",
+        createdAt: new Date().getTime()
+    })
 
-    socket.on('createMessage', (newMessage) =>{
-        console.log("Get a new Message From the client:", newMessage)
+
+    socket.on('createMessage', (message) =>{
+        console.log("Get a new Message From the client:", message)
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+        // socket.broadcast.emit('newMessage', {
+        //        from: message.from,
+        //        text: message.text,
+        //        createdAt: new Date().getTime()})
     })
 
     socket.on('disconnect', function(){
